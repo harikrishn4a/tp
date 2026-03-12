96214dfaab1e8bc6b630d022d5461a2ac892c70b
ashwacka <ashwikagupta@gmail.com>
2026-03-12 20:19:02 +0800

    Add Stage field to Person

--- src/main/java/seedu/address/model/person/Person.java
+++ src/main/java/seedu/address/model/person/Person.java
@@ -20,6 +20,7 @@ public class Person {
     private final Name name;
     private final Phone phone;
     private final Email email;
+    private final Stage stage;
 
     // Data fields
     private final Address address;
@@ -28,12 +29,13 @@ public class Person {
     /**
      * Every field must be present and not null.
      */
-    public Person(Name name, Phone phone, Email email, Address address, Set<Tag> tags) {
-        requireAllNonNull(name, phone, email, address, tags);
+    public Person(Name name, Phone phone, Email email, Address address, Stage stage, Set<Tag> tags) {
+        requireAllNonNull(name, phone, email, address, stage, tags);
         this.name = name;
         this.phone = phone;
         this.email = email;
         this.address = address;
+        this.stage = stage;
         this.tags.addAll(tags);
     }
 
@@ -51,6 +53,10 @@ public class Person {
 
     public Address getAddress() {
         return address;
+    }
+
+    public Stage getStage() {
+        return stage;
     }
 
     /**
@@ -94,13 +100,14 @@ public class Person {
                 && phone.equals(otherPerson.phone)
                 && email.equals(otherPerson.email)
                 && address.equals(otherPerson.address)
+                && stage.equals(otherPerson.stage)
                 && tags.equals(otherPerson.tags);
     }
 
     @Override
     public int hashCode() {
         // use this method for custom fields hashing instead of implementing your own
-        return Objects.hash(name, phone, email, address, tags);
+        return Objects.hash(name, phone, email, address, stage, tags);
     }
 
     @Override
@@ -110,6 +117,7 @@ public class Person {
                 .add("phone", phone)
                 .add("email", email)
                 .add("address", address)
+                .add("stage", stage)
                 .add("tags", tags)
                 .toString();
     }
--- src/test/java/seedu/address/testutil/PersonBuilder.java
+++ src/test/java/seedu/address/testutil/PersonBuilder.java
@@ -25,6 +25,7 @@ public class PersonBuilder {
     private Phone phone;
     private Email email;
     private Address address;
+    private seedu.address.model.person.Stage stage;
     private Set<Tag> tags;
 
     /**
@@ -35,6 +36,7 @@ public class PersonBuilder {
         phone = new Phone(DEFAULT_PHONE);
         email = new Email(DEFAULT_EMAIL);
         address = new Address(DEFAULT_ADDRESS);
+        stage = seedu.address.model.person.Stage.SURVEILLANCE;
         tags = new HashSet<>();
     }
 
@@ -46,6 +48,7 @@ public class PersonBuilder {
         phone = personToCopy.getPhone();
         email = personToCopy.getEmail();
         address = personToCopy.getAddress();
+        stage = personToCopy.getStage();
         tags = new HashSet<>(personToCopy.getTags());
     }
 
@@ -90,7 +93,7 @@ public class PersonBuilder {
     }
 
     public Person build() {
-        return new Person(name, phone, email, address, tags);
+        return new Person(name, phone, email, address, stage, tags);
     }
 
 }
